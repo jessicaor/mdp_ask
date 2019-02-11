4539d6aac0081899674dd708497cb878e113daa2
jessicaor <45964916+jessicaor@users.noreply.github.com>
2019-02-11 11:33:46 +0400

    Add files via upload

---
+++ test_secure/formulaire.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+
+  <form action="secret.php" method="post">
+    <input type="password" name="mdp"/>
+    <input type="submit" value="valider">
+  </form>
+
+  </body>
+ </html>
